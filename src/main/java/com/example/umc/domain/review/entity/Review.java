0fe7258f345134e27e9537e56c8980263a2e3ec6
HeejuKo <khjg00gle96@gmail.com>
2025-11-05 14:08:41 +0900

    ✨ [Feat] store 엔티티 설정

--- src/main/java/com/example/umc/domain/review/entity/Review.java
+++ src/main/java/com/example/umc/domain/review/entity/Review.java
@@ -1,4 +1,39 @@
 package com.example.umc.domain.review.entity;
 
-public class Review {
+import com.example.umc.domain.store.entity.Store;
+import com.example.umc.domain.user.entity.User;
+import com.example.umc.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "review")
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 작성자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // 어떤 가게에 대한 리뷰인지
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Column(nullable = false)
+    private Integer star;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
 }
--- src/main/java/com/example/umc/domain/store/entity/Store.java
+++ src/main/java/com/example/umc/domain/store/entity/Store.java
@@ -1,4 +1,44 @@
 package com.example.umc.domain.store.entity;
 
-public class Store {
+import com.example.umc.domain.mission.entity.Mission;
+import com.example.umc.domain.review.entity.Review;
+import com.example.umc.domain.store.entity.Category;
+import com.example.umc.domain.store.entity.Region;
+import com.example.umc.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "store")
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    // 이 가게에 달린 리뷰들
+    @OneToMany(mappedBy = "store", fetch = FetchType.LAZY)
+    private List<Review> reviews = new ArrayList<>();
+
+    // 이 가게가 가진 미션들
+    @OneToMany(mappedBy = "store", fetch = FetchType.LAZY)
+    private List<Mission> missions = new ArrayList<>();
 }
