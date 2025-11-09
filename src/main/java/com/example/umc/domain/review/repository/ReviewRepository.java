d0cbc60526d618468f80a0f20823bc83dc14380e
HeejuKo <khjg00gle96@gmail.com>
2025-11-10 03:11:54 +0900

    ✨ [Feat] Review 레포지토리 추가

--- src/main/java/com/example/umc/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/umc/domain/review/repository/ReviewRepository.java
@@ -1,4 +1,19 @@
 package com.example.umc.domain.review.repository;
 
-public class ReviewRepository {
+import com.example.umc.domain.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    // 특정 가게 리뷰 전체
+    @Query("SELECT r FROM Review r WHERE r.store.id = :storeId")
+    List<Review> findByStoreId(@Param("storeId") Long storeId);
+
+    // 특정 유저의 리뷰
+    @Query("SELECT r FROM Review r WHERE r.user.id = :userId")
+    List<Review> findByUserId(@Param("userId") Long userId);
 }
