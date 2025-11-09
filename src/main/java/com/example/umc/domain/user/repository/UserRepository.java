b49609f1a6285c685764e59d08da81d1d04eefee
HeejuKo <khjg00gle96@gmail.com>
2025-11-10 04:22:56 +0900

    ✨ [Feat] User
    [Feat] User

--- src/main/java/com/example/umc/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/umc/domain/user/repository/UserRepository.java
@@ -1,10 +1,22 @@
 package com.example.umc.domain.user.repository;
 
+import com.example.umc.domain.user.dto.MyPageDTO;
 import com.example.umc.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findById(Long id);
+    @Query("""
+        SELECT new com.example.umc.domain.user.dto.MyPageDTO(
+            u.nickname,
+            u.email,
+            u.phoneNum,
+            u.point
+        )
+        FROM User u
+        WHERE u.id = :userId
+    """)
+    MyPageDTO findMypageInfo(@Param("userId") Long userId);
 }
